fdde306e1a6ff72711725a1ba54d229d19d2a13b
zzc <z18351828988@163.com>
2023-08-25 18:11:13 +0800

    112233

--- src/com/zzc/receive_test/Test02.java
+++ src/com/zzc/receive_test/Test02.java
@@ -2,6 +2,6 @@ package com.zzc.receive_test;
 
 public class Test02 {
     public static void main(String[] args) {
-        System.out.println("Hello World123");
+        System.out.println("Hello World");
     }
 }
